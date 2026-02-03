e44e243ad3c5656c67b6854109093bc63390e59b
Kahraman Bayraktar <kah_ra_man@hotmail.com>
2026-02-03 15:53:53 +0300

    fix code based on CSharpier format rules

--- NexusSentinel.Web/Program.cs
+++ NexusSentinel.Web/Program.cs
@@ -7,17 +7,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 
 builder.Services.AddOutputCache();
 
 builder.Services.AddHttpClient<WeatherApiClient>(client =>
-    {
-        // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-        // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-        client.BaseAddress = new("https+http://apiservice");
-    });
+{
+    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+    client.BaseAddress = new("https+http://apiservice");
+});
 
 var app = builder.Build();
 
@@ -35,8 +34,7 @@ app.UseAntiforgery();
 
 app.UseOutputCache();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
 
 app.MapDefaultEndpoints();
 
